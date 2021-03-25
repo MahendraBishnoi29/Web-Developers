b783eebb0c6d71d6909be44949365644e4738334
Mahendra <mahendrabrowny@gmail.com>
2021-03-25 14:47:42 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+// //GET THE BUTTON
+// myButton = document.getElementById("myBtn");
+
+// //WHEN USER SCROLL DOWN FOR 20PX FROM TOP OF THE DOCUMENT , SHOW THE BUTTON
+// window.onscroll = function () {
+//   scrollFunction();
+// };
+
+// const scrollFunction = () => {
+//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//     myButton.style.display = "block";
+//   } else {
+//     myButton.style.display = "none";
+//   }
+//   //WHEN USER CLICK THAT BUTTON,SCROLL HIM TO TOP
+//   const topFunction = () => {
+//     document.body.scrollTop = 0;
+//     document.documentElement.scrollTop = 0;
+//   };
+// };
